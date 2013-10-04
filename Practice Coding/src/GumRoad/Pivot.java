10227bb5d3876471a5b8002990efc0826c8ac7d4
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2013-10-04 12:30:26 -0700

    Documenting

--- Practice Coding/src/GumRoad/Pivot.java
+++ Practice Coding/src/GumRoad/Pivot.java
@@ -3,13 +3,23 @@ package GumRoad;
 import java.util.Arrays;
 import java.util.List;
 
-/*
+/*Problem
  * Write a method that returns the "pivot" index of a list of integers.
  *  We define the pivot index as the index where the sum of the numbers on the left is equal to the sum of the numbers on the right.
  *  Given [1, 4, 6, 3, 2], the method should return 2, 
  *  since the sum of the numbers to the left of index 2 is equal to the sum of numbers to the right of index 2 (1 + 4 = 3 + 2). 
  *  If no such index exists, it should return -1.
  *  If there are multiple pivots, you can return the left-most pivot.
+ */
+/*Approach#1
+ * Assume pivot to be 1
+ * Eval left sum = #[0] and right sum #[2...n-1]
+ * Move the pivot right until 
+ * 		a. LeftSum = RightSum 	=> 	return pivot
+ * 		b. pivot == n 			=>	return -1
+ */
+/*Analysis
+ * O(n)*2 => O(n)
  */
 public class Pivot {
 
@@ -22,7 +32,7 @@ public class Pivot {
 		for(int i = pivotIndex+1; i<numbers.size(); i++) {
 			sigRight += numbers.get(i);
 		}
-		//Hack to enable a quick exit in case pivot swings between left and right moves
+		//TODO Hack to enable a quick exit in case pivot swings between left and right moves
 		boolean isPivotMovementRight = true;
 		
 		while(pivotIndex < numbers.size()-1) {
