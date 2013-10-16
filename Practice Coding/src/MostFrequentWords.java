b1be7c3bcef29bcf6e2d4ce292a543cdde294aa1
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2013-10-15 18:32:27 -0700

    Stopped.Partial implementation with guava - BiMaps. Realized the requirement for a 'standard' Java library

--- Practice Coding/src/MostFrequentWords.java
+++ Practice Coding/src/MostFrequentWords.java
@@ -6,20 +6,23 @@
  * Your solution should run in O(n) time where n is the number of characters in the document. 
  * Implement this function as you would for a production/commercial system
  */
-import com.google.common.collect.HashBiMap;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /*
  * Approaches:
- * 1. O(n) : Use a map to store word - frequency. 
- * 		a. O(n) time + space to build hashmap and b. O(n) time to find. Insertion/Deletion : O(1)
- * 2. O(nlogn) : Sort and count (O(n)). Space O(1). Insertion/Deletion : O(n)
+ * 1. O(n)(time and space) : Use a map to store word - frequency. 
+		Search:O(n). Insertion/Deletion : O(1)
+ * 2. O(nlogn) : Sort and count (O(n)). 
+ * 		Space O(1). Insertion/Deletion : O(n)
  * 3. List <Object> : Store String followed by freq. 
  * 		Building(from a Map) = O(n) time but space = 2n O(n). Find O(1). Insertion/Deletion : O(n). 
- * 4. BiMap : Guava library Insertion/Deletion : O(1)
+ * 4. BiMap : Guava library. Everything : O(1) => is not a standard lib!
  */
 public class MostFrequentWords {
+
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Input the string");
@@ -31,7 +34,7 @@ public class MostFrequentWords {
 	}
 
 	private static void setupFrequency(String hugeString, int n) {
-		HashBiMap<String,Integer> WordFreq = HashBiMap.create();
+		Map<String,Integer> wordFreq = new HashMap<String,Integer>();
 		char delimiter = ' ';
 		for(int i=0; i<hugeString.length(); i++) {
 			StringBuilder word = new StringBuilder("");
@@ -42,10 +45,10 @@ public class MostFrequentWords {
 					i++;
 					c=hugeString.charAt(i);
 				}
-				if(WordFreq.containsKey(word)) {
-					freq = WordFreq.get(word);
+				if(wordFreq.containsKey(word)) {
+					freq = wordFreq.get(word);
 			}
-			WordFreq.forcePut(word.toString(), freq);
+			wordFreq.put(word.toString(), freq);
 		}
 		//findNFrequent(WordFreq,n);
 	}
