81f8c735a8cb73dbc5f61fbf023951169775a598
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2013-10-31 15:37:32 -0700

    Removing temporary objects to replace with the original element - 4 byte int "tick". Adding time-stamp to output files

--- Practice Coding/src/ClockGranularity.java
+++ Practice Coding/src/ClockGranularity.java
@@ -1,58 +1,32 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.Semaphore;
 /*
  * Purpose is to test system clock granularity 
  */
-class testSeveralObjects {
-	byte byt;
-	int integer;
-	Integer INT;
-	byte[] bytes;
-	
-	public byte getByt() {
-		return byt;
-	}
-	public void setByt(byte byt) {
-		this.byt = byt;
-	}
-	public int getInteger() {
-		return integer;
-	}
-	public void setInteger(int integer) {
-		this.integer = integer;
-	}
-	public Integer getINT() {
-		return INT;
-	}
-	public void setINT(Integer iNT) {
-		INT = iNT;
-	}
-	public byte[] getBytes() {
-		return bytes;
-	}
-	public void setBytes(byte[] bytes) {
-		this.bytes = bytes;
-	}
-}
+
 public class ClockGranularity implements Runnable  {
 
-	private static final int nanoInMilli = 1000000;
-	
-	private static final int MaxExecutionTimeMsec = 60*1000;
+	private static final int MaxExecutionTimeMsec = 60000;
 	//60*1000 milliseconds
-	private static float delta =  0.01f ; /*Float => u-second calculations*/
+	private static float delta = 1; 
 	//Inter-arrival Time(IAT): in milliseconds
 	private static final int ArraySize =(int) ((float)MaxExecutionTimeMsec/delta);
-	static int tick = 0;
-	static Queue<Object> requestQueue = new LinkedList<Object>();
+	private static int tick = 0;
+	private static String outputFileName ="temp" + delta + " msec @" + getTime();
 	public static Thread t;
-	static long[] ticksCount = new long[ArraySize];
-	static Semaphore queueSemaphore = new Semaphore(1);
-	static byte[] o;
+	private static long[] ticksCount = new long[ArraySize];
+	
+	private static final int convertMilliToNano = 1000000;
+	private static Queue<Object> requestQueue = new LinkedList<Object>();
+	private static Semaphore queueSemaphore = new Semaphore(1);
+	private static byte[] o;
 	
 	public static void initTests() {
 		o = new byte[100000];
@@ -60,12 +34,17 @@ public class ClockGranularity implements Runnable  {
 			o[i] = 127;
 		}
 	}
+	private static String getTime() {
+		DateFormat format = new SimpleDateFormat("dd-MMM-yy HH:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		return format.format(cal.getTime());
+	}
 	@Override
 	public void run() {
 		try {
 			queueSemaphore.acquire();
 			tick++;
-			requestQueue.add(o);
+			requestQueue.add(tick);
 			queueSemaphore.release();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -77,7 +56,7 @@ public class ClockGranularity implements Runnable  {
 		t = new Thread(new ClockGranularity(),"lamda");
 		initTests();
 		//For nano secs:
-		ClockGranularity.delta = delta*nanoInMilli;
+		ClockGranularity.delta = delta*convertMilliToNano;
 		long execStartTime = System.currentTimeMillis();
 		long experimentStartTime = System.nanoTime();
 		long execDuration, experimentRuntime;
@@ -96,15 +75,15 @@ public class ClockGranularity implements Runnable  {
 			}
 		} while (execDuration <= MaxExecutionTimeMsec);
 		try {
-			output();
+			printToFile();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	private static void output() throws IOException{
+	private static void printToFile() throws IOException{
 		String lineSeparator = System.lineSeparator();
 		File directory = new File("C:\\Users\\Sagar\\git\\Brainteasing Code\\Practice Coding\\src\\ClockGranularity Test results\\Semaphore and Queue");
-		File file = File.createTempFile("Array of 100k:" + delta + " nanosec", ".txt",directory);
+		File file = File.createTempFile(outputFileName, ".txt",directory);
 		FileWriter writer = new FileWriter(file);
 		writer.append("Index\tTimestamp\tQueue Contents" + lineSeparator);
 		String summary = "tick:" + tick + ", tick[]" + ticksCount.length + ", queue<>" + requestQueue.size();
