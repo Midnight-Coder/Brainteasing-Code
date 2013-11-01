b5debd6d8afa64d4213d454dff9ddd2a4f771b02
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2013-11-01 11:25:13 -0700

    Evolved code for a sophisticated reader writer thread problem

--- Practice Coding/src/ClockGranularity.java
+++ Practice Coding/src/ClockGranularity.java
@@ -6,61 +6,40 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.Semaphore;
 /*
  * Purpose is to test system clock granularity 
  */
 
-public class ClockGranularity implements Runnable  {
 
+public class ClockGranularity{
 	private static final int MaxExecutionTimeMsec = 60000;
 	//60*1000 milliseconds
-	private static float delta = 1; 
+	private static float delta = 100; 
 	//Inter-arrival Time(IAT): in milliseconds
-	private static final int ArraySize =(int) ((float)MaxExecutionTimeMsec/delta);
-	private static int tick = 0;
-	private static String outputFileName ="temp" + delta + " msec @" + getTime();
-	public static Thread t;
+	static final int ArraySize =(int) ((float)MaxExecutionTimeMsec/delta);
+	static int tick = 0;
 	private static long[] ticksCount = new long[ArraySize];
-	
 	private static final int convertMilliToNano = 1000000;
-	private static Queue<Object> requestQueue = new LinkedList<Object>();
-	private static Semaphore queueSemaphore = new Semaphore(1);
-	private static byte[] o;
 	
-	public static void initTests() {
-		o = new byte[100000];
-		for(int i=0;i<o.length;i++) {
-			o[i] = 127;
-		}
-	}
 	private static String getTime() {
 		DateFormat format = new SimpleDateFormat("dd-MMM-yy HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
 		return format.format(cal.getTime());
 	}
-	@Override
-	public void run() {
-		try {
-			queueSemaphore.acquire();
-			tick++;
-			requestQueue.add(tick);
-			queueSemaphore.release();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		
-	}
+	/*
+	 * Invoke 1 producer vs 1,2,3 consumers
+	 * Write consumer to file
+	 */
 	public static void main(String args[]) {
-		t = new Thread(new ClockGranularity(),"lamda");
-		initTests();
-		//For nano secs:
 		ClockGranularity.delta = delta*convertMilliToNano;
 		long execStartTime = System.currentTimeMillis();
 		long experimentStartTime = System.nanoTime();
 		long execDuration, experimentRuntime;
-
+		Buffer requestQueue = new Buffer();
+		Thread producerThread = new Thread(new Producer(requestQueue));
+		Consumer consumer = new Consumer(requestQueue);
+		Thread consumerThread = new Thread(consumer);
+		consumerThread.start();
 		do {
 			execDuration = System.currentTimeMillis() - execStartTime;
 			experimentRuntime = System.nanoTime() - experimentStartTime;
@@ -71,22 +50,25 @@ public class ClockGranularity implements Runnable  {
 				}
 				else
 					ticksCount[tick] = execDuration;
-				t.run();
+			producerThread.run();	
 			}
 		} while (execDuration <= MaxExecutionTimeMsec);
 		try {
-			printToFile();
+			String producerFile = "Producer:" + delta + " msec @" + getTime();
+			printToFile(producerFile,requestQueue.getQueue());
+			String consumerFile = "Conumer:" + delta + " msec@" + getTime();
+			printToFile(consumerFile, consumer.getValidateConsumerArray());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	private static void printToFile() throws IOException{
+	public static void printToFile(String outputFileName,Queue<Integer> requestQueue) throws IOException{
 		String lineSeparator = System.lineSeparator();
 		File directory = new File("C:\\Users\\Sagar\\git\\Brainteasing Code\\Practice Coding\\src\\ClockGranularity Test results\\Semaphore and Queue");
 		File file = File.createTempFile(outputFileName, ".txt",directory);
 		FileWriter writer = new FileWriter(file);
 		writer.append("Index\tTimestamp\tQueue Contents" + lineSeparator);
-		String summary = "tick:" + tick + ", tick[]" + ticksCount.length + ", queue<>" + requestQueue.size();
+		String summary = "#tick:" + tick + ", tick[]" + ticksCount.length + ", queue<>" + requestQueue.size();
 		for(int i = 0; i<tick; i++) {
 			String temp = i + " ticks in " + ticksCount[i] + " msec\t" + "\t" + requestQueue.poll();
 			System.out.println(temp);
@@ -96,5 +78,55 @@ public class ClockGranularity implements Runnable  {
 		writer.append(summary + lineSeparator);
 		System.out.println(summary);
 		writer.close();
+	}
+}
+class Buffer {
+	private Queue<Integer> requestsQueue;
+	Buffer() {
+		requestsQueue = new LinkedList<Integer>();
+	}
+	public synchronized void put(Integer tick) {
+		//TODO include a while ?
+		requestsQueue.add(tick);
+	}
+	public synchronized int get() {
+		int tick;
+		while(requestsQueue.size() == 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		tick = requestsQueue.poll();
+		return tick;
+	}
+	public Queue<Integer> getQueue() {
+		return requestsQueue;
+	}
+}
+
+class Consumer implements Runnable{
+	private Buffer bufferQueue;
+	private Queue<Integer> validateConsumer;
+	Consumer(Buffer requestQueue) {
+		bufferQueue = requestQueue;
+		validateConsumer = new LinkedList<Integer>();
+	}
+	public void run() {
+		validateConsumer.add(bufferQueue.get());
+	}
+	public Queue<Integer> getValidateConsumerArray() {
+		return validateConsumer;
+	}
+}
+
+class Producer implements Runnable{
+	private Buffer bufferQueue;
+	Producer(Buffer requestQueue) {
+		bufferQueue = requestQueue;	
+	}
+	public void run() {
+		bufferQueue.put(ClockGranularity.tick);
 	}
 }
